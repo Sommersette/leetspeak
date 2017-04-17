5caae8a1316522f46b489358a081e8012730f35e
Nick Powell and Trevor Fast <nick@>
2017-04-17 16:53:29 -0700

    add rule for spaces that come before words

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -2,6 +2,7 @@ class String
   define_method(:leetspeak) do
     letter_holder = []
     characters = self.split("")
+    # i = 0
     characters.each() do |letter|
       if letter == "e"
         letter_holder.push(3)
@@ -10,12 +11,21 @@ class String
       elsif letter == "I" && letter != "i"
         letter_holder.push(1)
       elsif letter == "s"
-        letter_holder.push("z")
-      else
-        letter_holder.push(letter)
-      end
 
-    end
+    #     if characters[i-1] == " "
+    #
+    #     elsif i == 0
+    #
+    #     else
+    #       letter_holder.push("z")
+    #     end
+    #
+    #   else
+    #     letter_holder.push(letter)
+    #   end
+    #
+    # i = i + 1
+    # end
     letter_holder.join()
 
   end
